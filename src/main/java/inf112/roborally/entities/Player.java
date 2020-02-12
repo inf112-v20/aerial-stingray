7bf6fb6a675d35f92e7083e276e9b589807dc622
Maren Hundvin <maren.hundvin@student.uib.no>
2020-02-12 10:01:32 +0100

    Added that player can move using arrows, and made a show status function and a set life function.

--- src/main/java/inf112/roborally/entities/Player.java
+++ src/main/java/inf112/roborally/entities/Player.java
@@ -2,6 +2,8 @@ package inf112.roborally.entities;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.Vector2;
 
 /**
@@ -12,23 +14,71 @@ public class Player {
     // Coordinates
     private Vector2 pos;
 
+
     // Graphics
     private Texture texture;
-    private TextureRegion textureRegion;
+    private TextureRegion[][] textureRegion;
+
+    // Player icons
+    private TiledMapTileLayer.Cell player;
+    private TiledMapTileLayer.Cell playerDead;
+    private TiledMapTileLayer.Cell playerWon;
+
+    // Life, damage and flags
+    private int life = 3;
+    private int damage = 0;
+    private boolean flag1 = false;
+    private boolean flag2 = false;
+    private boolean flag3 = false;
+    private boolean flag4 = false;
+
 
     public Player(Vector2 pos) {
         this.pos = pos;
 
         // Graphics
         texture = new Texture("player.png");
-        textureRegion = TextureRegion.split(texture, 60, 60)[0][0];
+        textureRegion = TextureRegion.split(texture, 60, 60);
+
+        player = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(textureRegion[0][0]));
+        playerDead = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(textureRegion[0][1]));
+        playerWon = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(textureRegion[0][2]));
+
+        player.setTile(new StaticTiledMapTile(textureRegion[0][0]));
     }
 
-    public TextureRegion getTextureRegion() {
-        return textureRegion;
+    //public TextureRegion getTextureRegion() {
+    //    return textureRegion;
+    //}
+
+    public TiledMapTileLayer.Cell getPlayerIcon() {
+        return player;
     }
 
-    public Vector2 getPos() {
-        return pos;
+    public TiledMapTileLayer.Cell getDeadPlayerIcon() { return playerDead; }
+
+    public TiledMapTileLayer.Cell getWonPlayerIcon() {
+        return playerWon;
     }
+
+    public Vector2 getPos() { return pos; }
+
+    public String showStatus(){
+        if (life <= 0) return "You are dead";
+        String str = "Life: " + life + ", Damage: " + damage;
+        if (flag3)
+            str += "\n You have 3 flags";
+        else if (flag2)
+            str += "\n You have 2 flags";
+        else if (flag1)
+            str += "\n You have flag 1";
+
+        return str;
+    }
+
+    public void setLife() {
+        this.life --;
+    }
+
+
 }
--- src/main/java/inf112/roborally/ui/Board.java
+++ src/main/java/inf112/roborally/ui/Board.java
@@ -2,18 +2,21 @@ package inf112.roborally.ui;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import inf112.roborally.entities.Player;
 
-public class Board implements ApplicationListener {
+public class Board extends InputAdapter implements ApplicationListener{
 
     // Misc
     public static final int TILE_SIZE = 60;  // Size of each tile in width & height
@@ -23,11 +26,17 @@ public class Board implements ApplicationListener {
     private TiledMapRenderer mapRenderer;
     private OrthographicCamera camera;
 
+    // Map layers
+    TiledMapTileLayer playerLayer;
+    TiledMapTileLayer holeLayer;
+    TiledMapTileLayer conLeft;
+
     // Rendering
     private SpriteBatch batch;
     private BitmapFont font;
 
     private Player player;
+    TiledMapTileLayer.Cell playerIcon;
 
     @Override
     public void create() {
@@ -35,15 +44,66 @@ public class Board implements ApplicationListener {
         batch = new SpriteBatch();
         font = new BitmapFont();
 
+        Gdx.input.setInputProcessor(this);
+
         // Map
         map = new TmxMapLoader().load("Map.tmx");
+        playerLayer = (TiledMapTileLayer) map.getLayers().get("Player");
+        holeLayer = (TiledMapTileLayer) map.getLayers().get("Hole");
+        conLeft = (TiledMapTileLayer) map.getLayers().get("ConLeft");
+
+
         mapRenderer = new OrthogonalTiledMapRenderer(map);
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         camera.update();
 
         player = new Player(new Vector2(13, 1));
+        playerIcon = player.getPlayerIcon();
+
     }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        boolean moved = false;
+        int x = (int) player.getPos().x;
+        int y = (int) player.getPos().y;
+
+        if (keycode == Input.Keys.UP) {
+            player.getPos().y += 1;
+            moved = true;
+        } else if (keycode == Input.Keys.DOWN){
+            player.getPos().y -= 1;
+            moved = true;
+        }else if(keycode ==Input.Keys.LEFT){
+            player.getPos().x -=1;
+            moved = true;
+        }else if (keycode == Input.Keys.RIGHT) {
+            player.getPos().x += 1;
+            moved = true;
+        }
+
+        if (moved) {
+            playerLayer.setCell(x, y, null);
+            playerIcon = player.getPlayerIcon();
+            System.out.println(player.showStatus());
+            activateBoard();
+            return true;
+        }
+        return false;
+    }
+
+    private void activateBoard(){
+        if (holeLayer.getCell((int) player.getPos().x, (int) player.getPos().y) != null){
+            playerIcon = player.getDeadPlayerIcon();
+            player.setLife();
+        }else{
+            if (conLeft.getCell((int) player.getPos().x, (int) player.getPos().y) != null) {
+                player.getPos().x--;
+            }
+        }
+    }
+
 
     @Override
     public void dispose() {
@@ -60,7 +120,15 @@ public class Board implements ApplicationListener {
         mapRenderer.setView(camera);
         mapRenderer.render();
 
-        drawPlayer(player);
+
+        //if (holeLayer.getCell((int) player.getPos().x, (int) player.getPos().y) != null){
+        //    playerLayer.setCell((int) player.getPos().x, (int) player.getPos().y, player.getDeadPlayerIcon());
+        //}else
+        //    playerLayer.setCell((int) player.getPos().x, (int) player.getPos().y, player.getPlayerIcon());
+
+
+        playerLayer.setCell((int) player.getPos().x, (int) player.getPos().y, playerIcon);
+        //drawPlayer(player);
     }
 
     /**
@@ -69,9 +137,9 @@ public class Board implements ApplicationListener {
      * @param player The entity to be put
      */
     public void drawPlayer(Player player) {
-        batch.begin();
-        batch.draw(player.getTextureRegion(), player.getPos().x * TILE_SIZE, player.getPos().y * TILE_SIZE);
-        batch.end();
+        //batch.begin();
+        //batch.draw(player.getTextureRegion(), player.getPos().x * TILE_SIZE, player.getPos().y * TILE_SIZE);
+        //batch.end();
     }
 
     @Override
