8bd58de53dea37aa8d0a50c9b083dd2d839d8923
zipsap <larsjuv@gmail.com>
2020-02-23 17:48:00 +0100

    Added tests for Player.java

---
+++ src/test/java/inf112/roborally/tests/PlayerTest.java
@@ -0,0 +1,32 @@
+package inf112.roborally.tests;
+
+import com.badlogic.gdx.math.Vector2;
+import inf112.roborally.entities.Player;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class PlayerTest {
+
+    Vector2 playerPos;
+    Player player;
+
+    @Before
+    public void initialize() {
+        playerPos = new Vector2(10, 10);
+        player = new Player(playerPos);
+    }
+
+    @Test
+    public void testGetPos() {
+        assertEquals(player.getPos(), playerPos);
+    }
+
+    @Test
+    public void testHasAllFlags() {
+        // Should not have all flags at constructor
+        assertFalse(player.hasAllFlags());
+    }
+}
