2fd0138c254c4652a7ec78eaf1a182a615dd793c
Maren Hundvin <maren.hundvin@student.uib.no>
2020-04-15 10:19:40 +0200

    Removed unused function RoboRally.run()

--- src/main/java/inf112/roborally/screens/RoboRally.java
+++ src/main/java/inf112/roborally/screens/RoboRally.java
@@ -332,13 +332,6 @@ public class RoboRally implements Screen {
         Gdx.input.setInputProcessor(stage);
     }
 
-    /*
-    private void run(){
-        for (Player player : players) {
-            EventUtil.handleEvent(board, player, players);
-        }
-    }
-    */
 
     /**
      * Locks in the selected cards and furthers the process
